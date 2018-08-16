769eeb11ff5f66dba10b601b66922e98e80e54bb
渣渣辉 <1@fxcdev.com>
2018-08-16 16:23:37 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@
 set -eo pipefail
 
 app_name="space-vim"
-repo_uri="https://github.com/emrysf/space-vim.git"
+repo_uri="https://github.com/emrysf/vim-config.git"
 repo_name="space-vim"
 repo_path="$HOME/.space-vim"
 repo_branch="master"
